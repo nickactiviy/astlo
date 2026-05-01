51ebdd89d1a0d17a5b80ddac8215c7eb4d46459e
Nick <121074947+nickactiviy@users.noreply.github.com>
2026-05-01 19:47:51 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import rehypeLazyLoadImage from './src/plugins/lazyLoadImage.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://nike.nicoleleta.workers.dev',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex, rehypeLazyLoadImage],
